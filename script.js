55c3a090baadf5700ac96abcd38e36fbba157ae4
Ronny Ismael <ronny.ismael@sjsu.edu>
2023-11-09 10:49:11 -0800

    updated contact

--- script.js
+++ script.js
@@ -23,3 +23,24 @@ function closemenu() {
   var sidemenu = document.getElementById("sidemenu");
   sidemenu.style.right = "-200px";
 }
+
+// Function to submit form to google sheet
+document.addEventListener('DOMContentLoaded', function() {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxtUj7LvKp-7IOazihKXp2vh62_6WIXZhi_YY88cP-P3kwu1t3IYbIAUnodKelUyzYN/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById('msg')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message Sent!"
+        setTimeout(function(){ msg.innerHTML = "" }, 5000);
+        form.reset()
+      })
+      .catch(error => {
+        msg.innerHTML = "FAIL TO SEND MESSAGE!"
+        setTimeout(function(){ msg.innerHTML = "" }, 5000);
+        form.reset()
+      })
+  })
+});
